f01e93907ae6bae4f24100232d0623c79ecfe46b
ynonp <ynonperek@gmail.com>
2022-06-01 11:17:06 +0300

    painter step 2

--- day3/multi-painter/src/App.jsx
+++ day3/multi-painter/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Painter from './painter';
 
+// Repo is here:
+// https://github.com/ynonp/frontend-2022-05/
+
+// (*) talk about inline style
+
 const image = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 4, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 1, 1, 1, 0, 0],
     [0, 1, 0, 0, 1, 1, 0, 0],
--- day3/multi-painter/src/painter.jsx
+++ day3/multi-painter/src/painter.jsx
@@ -1,10 +1,36 @@
 
+function palette(value) {
+    return [
+        'white',
+        'black',
+        'red',
+        'green',
+        '#2202AA',
+    ][value];
+}
+
 export default function Painter(props) {
     const { image } = props;
 
     return (
         <div>
             <pre>{JSON.stringify(image)}</pre>
+            <div className="image">
+            {
+                image.map((line, lineIndex) => (
+                    line.map((square, columnIndex) => (
+                        // <div className={`square ${palette(square)}`} ></div>
+                        <div
+                            className="square"
+                            key={`${lineIndex}-${columnIndex}`}
+                            style={{
+                                backgroundColor: palette(square),
+                            }}
+                        />
+                    ))
+                ))
+            }
+            </div>
         </div>
     )
 }
