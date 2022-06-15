ef5ce097a08ea11c5c9c21ee26e6c1cb94115d33
ynonp <ynonperek@gmail.com>
2022-06-15 15:23:11 +0300

    fixed html

--- day5/demo-client-server/dig/src/App.jsx
+++ day5/demo-client-server/dig/src/App.jsx
@@ -2,15 +2,13 @@ function App() {
   return (
     <div>
       <h1>Your Job</h1>
-      <p>
-        <ul>
-          <li><input type="checkbox" />Implement the code so adding a bookmark will create a new item in the list</li>
-          <li><input type="checkbox" />Bookmarks will be shared between clients (using the server)</li>
-          <li><input type="checkbox" />Server will save all bookmarks to a local file</li>
-          <li><input type="checkbox" />When a client starts, they will get all the data from the file (on the server)</li>
-          <li><input type="checkbox" />Implement +1 and -1, and also update the file</li>
-        </ul>
-      </p>
+      <ul>
+        <li><input type="checkbox" />Implement the code so adding a bookmark will create a new item in the list</li>
+        <li><input type="checkbox" />Bookmarks will be shared between clients (using the server)</li>
+        <li><input type="checkbox" />Server will save all bookmarks to a local file</li>
+        <li><input type="checkbox" />When a client starts, they will get all the data from the file (on the server)</li>
+        <li><input type="checkbox" />Implement +1 and -1, and also update the file</li>
+      </ul>
       <h1>Bookmark Share</h1>
       <div>
         <label>
