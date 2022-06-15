4e5e0e6bf2fb29b6c3ef204f43a6a5f1b51c9654
ynonp <ynonperek@gmail.com>
2022-06-15 15:32:26 +0300

    add connect/disconnect handlers to server

--- day5/demo-client-server/server/server.py
+++ day5/demo-client-server/server/server.py
@@ -6,6 +6,16 @@ app = web.Application()
 
 sio.attach(app)
 
+# Called when a new client connects
+@sio.event
+def connect(sid, environ, auth):
+    print('connect ', sid)
+
+# Called when a client leaves
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
 @sio.on('message')
 async def print_message(sid, message):
     print("Socket ID: " , sid)
